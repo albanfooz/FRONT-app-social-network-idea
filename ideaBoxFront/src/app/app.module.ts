3f5b73b7c5f631c8c5504c058a6ca16a4ab788a7
Gaelle <gaelleoger@hotmail.fr>
2019-10-31 15:42:51 +0100

    j'arrete les betises

--- ideaBoxFront/src/app/app.module.ts
+++ ideaBoxFront/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { BarIconsIdeeComponent } from './bar-icons-idee/bar-icons-idee.component';
+import { BarIconsIdeeComponent } from './components/bar-icons-idee/bar-icons-idee.component';
 import { FrontPageComponent } from './components/front-page/front-page.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
