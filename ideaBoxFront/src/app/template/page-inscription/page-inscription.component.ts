56e4c215345713a98e454fe693de405385174d1b
Alban <Alban.fooz@gmail.com>
2019-11-05 16:20:53 +0100

    Auto stash before revert of "Merge branch 'develop' into feature/MainPage2"

---
+++ ideaBoxFront/src/app/template/page-inscription/page-inscription.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-inscription',
+  templateUrl: './page-inscription.component.html',
+  styleUrls: ['./page-inscription.component.css']
+})
+export class PageInscriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
