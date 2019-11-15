24b8406d5ef7df9f2bf38a6f1599c3189505fc64
Paul <maujean.paul@gmail.com>
2019-11-15 11:36:09 +0100

    Ajout lien "Nouvelle idée"

--- ideaBoxFront/src/app/components/nav/nav.component.ts
+++ ideaBoxFront/src/app/components/nav/nav.component.ts
@@ -11,9 +11,7 @@ import { map, shareReplay } from 'rxjs/operators';
 export class NavComponent {
 
   links = [
-    { pageName: 'Link', pageLink: '/Link' },
-    { pageName: 'Zelda', pageLink: '/Zelda' },
-    { pageName: 'Ganondorf', pageLink: '/Ganondorf' }
+    { name: 'Nouvelle idée', url: '/ajout-idee', icon: 'edit' }
   ];
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
