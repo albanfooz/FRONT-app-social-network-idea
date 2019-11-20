9815b550d092411ff20056868a078a45cae39f45
luc <luckiminus@hotmail.fr>
2019-11-20 15:13:38 +0100

    mise en forme inscription v2

--- ideaBoxFront/src/app/pages/page-inscription/page-inscription.component.ts
+++ ideaBoxFront/src/app/pages/page-inscription/page-inscription.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { Route, Router } from '@angular/router';
 import { ProfilModel } from 'src/app/models/ProfilModel';
 import { InscriptionService } from 'src/app/services/InscriptionServices';
@@ -10,19 +10,20 @@ import { InscriptionService } from 'src/app/services/InscriptionServices';
   styleUrls: ['./page-inscription.component.css']
 })
 export class PageInscriptionComponent implements OnInit {
-  creationInscriptionForm = this.fb.group({
-    _nom: [null],
-    _prenom: [null],
-    _mail: [null, Validators.required],
-    _pseudo: [null, Validators.required],
-    _motDePasse: [null, Validators.required]
-  });
-
+  creationInscriptionForm: FormGroup;
   hasUnitNumber = false;
 
   private inscription: ProfilModel;
 
-  constructor(private fb: FormBuilder, private InscriptionService: InscriptionService, private router: Router) { }
+  constructor(private fb: FormBuilder, private InscriptionService: InscriptionService, private router: Router) {
+    this.creationInscriptionForm = this.fb.group({
+      _nom: [null],
+      _prenom: [null],
+      _mail: [null, Validators.required],
+      _pseudo: [null, Validators.required],
+      _motDePasse: [null, Validators.required]
+    });
+  }
 
 
   ngOnInit() {
