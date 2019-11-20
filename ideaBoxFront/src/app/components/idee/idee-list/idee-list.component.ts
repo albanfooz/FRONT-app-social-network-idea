7f33cd21a7e2fcdd42c7e082f3dad4613274c4a8
Paul <maujean.paul@gmail.com>
2019-11-20 11:32:14 +0100

    Infinite Scroll MainPage terminé

--- ideaBoxFront/src/app/components/idee/idee-list/idee-list.component.ts
+++ ideaBoxFront/src/app/components/idee/idee-list/idee-list.component.ts
@@ -7,10 +7,41 @@ import { IdeeModel } from 'src/app/models/IdeeModel';
   styleUrls: ['./idee-list.component.css']
 })
 export class IdeeListComponent implements OnInit {
+
+  scrollDistance = 1;
+  scrollThrottle = 150;
+
+  private perPage = 5;
+  private nbrIdees = 0;
+
+  private displayedIdees = new Array<IdeeModel>();
+
   @Input() ideesList: Array<IdeeModel>;
+
   constructor() { }
 
+  private refreshDisplayedIdees() {
+
+    let i = this.nbrIdees;
+    this.nbrIdees += this.perPage;
+
+    for (i; i < this.nbrIdees; i++) {
+
+      if (this.ideesList[i]) {
+        this.displayedIdees.push(this.ideesList[i]);
+      }
+
+    }
+  }
+
+  onScrollDown() {
+
+    this.refreshDisplayedIdees();
+    console.log('Displayed Ideas = ' + this.nbrIdees);
+  }
   ngOnInit() {
+
+    this.refreshDisplayedIdees();
   }
 
 }
